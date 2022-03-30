311462924e7b4627036ca38f837f2fa76a065ba4
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-03-29 21:29:38 -0300

    Aplicativo de previsão do tempo desenvolvido com React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = 'https://api.hgbrasil.com/weather?woeid=455823&format=json-cors&locale=pt';// woeid é de Porto Alegre
+
+export default class App extends React.Component {
+  state = {
+    city: "Carregando...",
+    forecast: []
+  }
+
+  componentDidMount(){
+    axios.get(API)
+      .then(({data}) => {
+        this.setState({
+          city: data.results.city_name,
+          forecast: data.results.forecast
+        });
+      });
+  }
+  
+  render(){
+    return (
+      <div className="container centered">
+        <h1>{this.state.city}</h1>
+        <table>
+          <thead>
+            <tr>
+            <th>Data</th>
+            <th>Min.</th>
+            <th>Max.</th>
+            <th>Previsão</th>
+            <th>Ícone</th>
+            </tr>
+          </thead>
+          <tbody>
+           {
+             this.state.forecast.map((day, index)=> {
+               return (
+                <tr key={day.date}>
+                  <td>{day.date}</td>
+                  <td>{day.min}</td>
+                  <td>{day.max}</td>
+                  <td>{day.description}</td>
+                  <td><img src={`/weather-icons/${day.condition}.svg`} alt={day.description}/></td>
+                </tr>
+               );
+             })
+           }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default App;
